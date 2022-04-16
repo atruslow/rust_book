3e93fa75593699f97daa679f987b3f13db811319
Alexander Truslow <alextruslow@gmail.com>
2022-04-16 07:41:18 -0400

    Working through chapter three

--- chapter_three_practice/f_to_c.rs
+++ chapter_three_practice/f_to_c.rs
@@ -7,18 +7,17 @@ fn main() {
         io::stdin()
             .read_line(&mut temp)
             .expect("failed to read line");
-        
+
         let temp: f64 = match temp.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
         println!("Your temp in C is {}", c_to_f(temp));
-        break
+        break;
     }
-
 }
 
-fn c_to_f(f:f64) -> f64 {
-    ((f-32.0) * 5.0/9.0).round()
+fn c_to_f(f: f64) -> f64 {
+    ((f - 32.0) * 5.0 / 9.0).round()
 }
